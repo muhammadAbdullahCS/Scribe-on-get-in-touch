1d8003333409fc0a4e24b97d3dd8032f73d7b1a5
muhammadAbdullahCS <muhammad.abdullah.cs01@gmail.com>
2024-12-12 16:09:56 +0500

    form responsiveness is done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -73,19 +73,19 @@ function Contact() {
   return (
     <>
       <Navbar mode="dark" />
-      <div className="flex flex-col justify-center items-start">
+      <div className="flex flex-col justify-center items-start max-sm:justify-start max-md:justify-start max-lg:justify-start hd:justify-start 4k2:justify-start">
         <div
-          className="w-full  4k2:h-[62rem] bg-[#0D002D] relative 
-                    overflow-hidden flex justify-center items-center flex-col gap-7  max-sm:h-full"
+          className="w-full h-[120vh] max-sm:h-[150vh] max-md:h-[150vh]  4k2:h-[100vh] bg-[#0D002D] relative 
+                    overflow-hidden flex justify-center max-sm:justify-start max-md:justify-start max-lg:justify-start hd:justify-start 4k2:justify-start items-center flex-col gap-7  "
         >
           <BlobAnimation blobs={blobs} style={{ zIndex: -1 }} />
 
-          <div className="flex flex-col h-full justify-center w-[70%] max-sm:w-[90%] gap-10">
+          <div className="flex flex-col h-full justify-center max-sm:justify-start max-md:justify-start max-lg:justify-start hd:justify-start 4k2:justify-start  w-[70%] max-sm:w-[90%] gap-10 max-sm:pt-10 max-md:pt-10 max-lg:pt-10 hd:pt-20">
                     <div className=" z-20">
-                            <h1 className="text-left font-playfair text-2xl sm:text-1xl md:text-4xl lg:text-4xl xl:text-5xl hd:text-5xl 4k:text-6xl 4k:leading-snug hd:leading-tight md:leading-[1.15] 4k2:text-9xl 4k2:leading-tight w-auto mb-9">
+                            <h1 className="text-left font-playfair text-1xl max-sm:text-2xl max-md:text-xl  sm:text-1xl md:text-4xl lg:text-4xl xl:text-5xl hd:text-5xl 4k:text-6xl 4k:leading-snug hd:leading-tight md:leading-[1.15] 4k2:text-9xl 4k2:leading-tight w-auto mb-9">
                             Get in Touch for Tailored Transcription Solutions.
                             </h1>
-                            <p className="text-left font-raleway text-1xl md:text-3xl 4k:text-4xl 4k:leading-snug 4k2:text-7xl 4k2:leading-tight ">
+                            <p className="text-left font-raleway text-1xl max-sm:text-lg max-md:text-base max-lg:text-lg md:text-3xl 4k:text-4xl 4k:leading-snug 4k2:text-7xl 4k2:leading-tight ">
                             Our team is ready to provide customised services that meet your
                             unique needs. Let's discuss how we can help you reclaim valuable
                             time and enhance your practice.
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,58 +3,59 @@ import React from 'react'
 function Form() {
   return (
     <>
-    <div className=" border border-white w-[60%] max-sm:w-full h-full max-sm:h-[45%]  z-10 relative bg-white text-black rounded-3xl flex flex-col justify- e items-start pl-8 py-8 gap-5 ">
-      <div className='flex flex-col gap-1'>
-            <h1 className=''> <b>Contact Form</b></h1>
-            <p className=''>Please fill out the form below, and we'll get back to you within 24 hours</p>
+    <div className=" border border-white w-[80%] max-sm:w-[98%] max-lg:w-[80%] max-md:w-[80%] 4k2:w-[60%] h-[60vh] max-sm:h-[70%] max-md:h-[80%] 4k2:h-[60%] z-10 relative bg-white text-black rounded-3xl flex flex-col justify- e items-start pl-8 py-8 4k2:pl-16 4k2:py-16 gap-5 max-sm:gap-0  ">
+
+      <div className='flex flex-col gap-1 max-sm:h-[15%] max-md:h-[10%] max-lg:h-[20%] hd:h-[20%] max-sm:w-[80%] max-md:w-[80%] 4k2:gap-5'>
+            <h1 className='max-sm:text-2lg max-lg:text-lg hd:text-3xl 4k2:text-6xl'> <b>Contact Form</b></h1>
+            <p className='max-sm:text-base max-lg:text-lg hd:text-2xl hd:w-[80%] 4k2:text-5xl'>Please fill out the form below, and we'll get back to you within 24 hours</p>
       </div>
         
-                <div className='flex flex-row flex-wrap gap-10 h-[15rem]'>
+                <div className='input_txt_container flex flex-row flex-wrap gap-7 h-[80%] max-sm:h-[85%] max-md:h-[90%] max-lg:h-[80%] hd:h-[80%] max-lg:gap-7 hd:gap-20'>
 
-                        <div className="input_container flex flex-col h-auto justify-between max-sm:gap-2">
+                        <div className="input_container flex flex-col h-auto justify-between max-sm:gap-1 ">
 
-                              <div className='flex flex-col h-auto '>
-                                <label htmlFor="" className=''>Full Name</label>
-                                <input type="text" className='border border-gray-400 w-52 rounded'/>
+                              <div className='flex flex-col h-auto hd:gap-3 '>
+                                <label htmlFor="" className='max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>Full Name</label>
+                                <input type="text" className='border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded'/>
                               </div>
 
-                                      <div className='flex flex-col h-auto '>
-                                        <label htmlFor="">Email</label>
-                                        <input type="text" className='border border-gray-400 w-52 rounded'/>
+                                      <div className='flex flex-col h-auto hd:gap-3 '>
+                                        <label htmlFor="" className='max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>Email</label>
+                                        <input type="text" className='border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded'/>
                                       </div>
 
-                                            <div className='flex flex-col h-auto '>
-                                              <label htmlFor="">Phone Number</label>
-                                              <input type="text" className='border border-gray-400 w-52 rounded'/>
+                                            <div className='flex flex-col h-auto hd:gap-3 '>
+                                              <label htmlFor="" className='max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>Phone Number</label>
+                                              <input type="text" className='border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded'/>
                                             </div>
 
-                              <div className='flex flex-col h-auto '>
-                                <label htmlFor="">Company/Organisation</label>
-                                <input type="text" className='border border-gray-400 w-52 rounded'/>
+                              <div className='flex flex-col h-auto hd:gap-3 '>
+                                <label htmlFor="" className='max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>Company/Organisation</label>
+                                <input type="text" className='border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded'/>
                               </div>
                         </div>
 
-                        <div className="txt_container flex flex-col justify-between max-sm:gap-2">
-                          <p>interested in:</p>
-                          <p>Select all that apply</p>
+                        <div className="txt_container flex flex-col justify-between max-sm:gap-1 max-sm:w-[90%]">
+                          <p className='max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>interested in:</p>
+                          <p className='max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>Select all that apply</p>
                           
                               <div className=' flex gap-1 items-center justify-start'>
                                   <input type='checkbox'  />
-                                  <p><b className='max-sm:text-sm'>Customised Transcription Services</b></p>
+                                  <p><b className='max-sm:text-base hd:text-2xl 4k2:text-5xl'>Customised Transcription Services</b></p>
                               </div>
 
                                     <div className='flex gap-1 items-center justify-start' >
                                         <input type='checkbox' />
-                                      <p><b className='max-sm:text-sm'>Request a Demo</b></p>
+                                      <p><b className='max-sm:text-base hd:text-2xl 4k2:text-5xl'>Request a Demo</b></p>
                                     </div>
 
                                           <div className=' flex gap-1 items-center justify-start'>
                                               <input type='checkbox' />
-                                            <p><b className='max-sm:text-sm'>Other</b></p>
+                                            <p><b className='max-sm:text-base hd:text-2xl 4k2:text-5xl'>Other</b></p>
                                           </div>
-                          <textarea className='text-black border border-gray-500 rounded' cols="auto" rows="auto"></textarea>
+                          <textarea className='text-black border border-gray-500 rounded 4k2:h-[9rem]' cols="auto" rows="auto"></textarea>
                           
-                          <button className='w-[40%]  text-white rounded-lg bg-black py-2 px-2'>Submit</button>
+                          <button className='w-[40%] 4k2:text-5xl hd:text-2xl text-white rounded-lg bg-black py-2 px-2 4k2:py-6'>Submit</button>
 
 
                         </div>
