6fb3504c448d1c8b44968af983765a26e6c9e419
muhammadAbdullahCS <muhammad.abdullah.cs01@gmail.com>
2024-12-13 16:19:18 +0500

    resolve Form responsive errors

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -74,66 +74,66 @@ function Contact() {
     <>
       <Navbar mode="dark" />
 
-{/* ---------------------------------------------form section ------------------------------------- */}
-      <div className="flex flex-col justify-center items-start max-sm:justify-start max-md:justify-start max-lg:justify-start hd:justify-start 4k2:justify-start">
+      {/* ---------------------------------------------form section ------------------------------------- */}
+      <div className="flex flex-col justify-start items-start max-sm:justify-start max-md:justify-start max-lg:justify-start hd:justify-start 4k2:justify-start">
         <div
-          className="w-full h-[160vh] max-sm:h-[200vh] max-md:h-[150vh]  4k2:h-[100vh] bg-[#0D002D] relative 
-                    overflow-hidden flex justify-center max-sm:justify-start max-md:justify-start max-lg:justify-start hd:justify-start 4k2:justify-start items-center flex-col gap-7  "
+          className="w-full h-auto bg-[#0D002D] relative 
+                    overflow-hidden flex justify-start max-sm:justify-start max-md:justify-start max-lg:justify-start hd:justify-start 4k2:justify-start items-center flex-col gap-7 md:pb-28 max-sm:pb-10 "
         >
           <BlobAnimation blobs={blobs} style={{ zIndex: -1 }} />
 
-          <div className="flex flex-col h-full justify-center max-sm:justify-start max-md:justify-start max-lg:justify-start hd:justify-start 4k2:justify-start  w-[70%] max-sm:w-[90%] gap-10 max-sm:pt-10 max-md:pt-10 max-lg:pt-10 hd:pt-20">
-                    <div className=" z-20">
-                            <h1 className="text-left font-playfair text-1xl max-sm:text-2xl max-md:text-xl  sm:text-1xl md:text-4xl lg:text-4xl xl:text-5xl hd:text-5xl 4k:text-6xl 4k:leading-snug hd:leading-tight md:leading-[1.15] 4k2:text-9xl 4k2:leading-tight w-auto mb-9">
-                            Get in Touch for Tailored Transcription Solutions.
-                            </h1>
-                            <p className="text-left font-raleway text-1xl max-sm:text-lg max-md:text-base max-lg:text-lg md:text-3xl 4k:text-4xl 4k:leading-snug 4k2:text-7xl 4k2:leading-tight ">
-                            Our team is ready to provide customised services that meet your
-                            unique needs. Let's discuss how we can help you reclaim valuable
-                            time and enhance your practice.
-                            </p>
-                    </div>
-                    <Form />
+          <div className="flex flex-col h-full justify-start max-sm:justify-start max-md:justify-start max-lg:justify-start hd:justify-start 4k2:justify-start  w-[70%] max-sm:w-[90%] max-md:w-[90%] gap-10 pt-20 max-sm:pt-10 max-md:pt-10 max-lg:pt-10 hd:pt-20">
+            <div className=" z-20">
+              <h1 className="text-left font-playfair text-1xl max-sm:text-2xl max-md:text-xl  sm:text-1xl md:text-4xl lg:text-4xl xl:text-5xl hd:text-5xl 4k:text-6xl 4k:leading-snug hd:leading-tight md:leading-[1.15] 4k2:text-9xl 4k2:leading-tight w-auto mb-9">
+                Get in Touch for Tailored Transcription Solutions.
+              </h1>
+              <p className="text-left font-raleway text-1xl max-sm:text-lg max-md:text-base max-lg:text-lg md:text-3xl 4k:text-4xl 4k:leading-snug 4k2:text-7xl 4k2:leading-tight ">
+                Our team is ready to provide customised services that meet your
+                unique needs. Let's discuss how we can help you reclaim valuable
+                time and enhance your practice.
+              </p>
+            </div>
+            <Form />
           </div>
-          
         </div>
       </div>
 
-{/* --------------------------------------------- prefer to speak section ------------------------------------- */}
+      {/* --------------------------------------------- prefer to speak section ------------------------------------- */}
 
-<div className="w-full flex justify-center ">
-    <div className="container w-[60%] flex flex-col  border-2 border-solid border-black ">
+      <div className="w-full flex justify-center ">
+        <div className="container w-[60%] flex flex-col  border-2 border-solid border-black ">
           <div className="prefer_part text-black flex flex-col  py-10 gap-3 ">
-                      <div className="gap-5">
-                        <h1 className="text-2xl md:text-3xl lg:text-4xl hd:text-6xl 4k2:text-9xl font-playfair text-transparent font-bold 
-                                        bg-clip-text bg-gradient-to-l from-[#5857F6] to-[#101052]">
-                                            We Scribe On so you can carry on
-                                        </h1>
-                          <p>
-                            Get in touch with us now.
-                                                  </p>
-                      </div>
-
-                      <button className="font-raleway w-[40%] 4k2:text-5xl hd:text-2xl text-white rounded-lg bg-black py-2 px-2 4k2:py-6">Start Live Chat Now</button>
+            <div className="gap-5">
+              <h1
+                className="text-2xl md:text-3xl lg:text-4xl hd:text-6xl 4k2:text-9xl font-playfair text-transparent font-bold 
+                                         bg-clip-text bg-gradient-to-l from-[#5857F6] to-[#101052]"
+              >
+                We Scribe On so you can carry on
+              </h1>
+              <p>Get in touch with us now.</p>
             </div>
 
-            <div className="why_contact text-black gap-3  ">
-                    <h1 className="text-2xl md:text-3xl lg:text-4xl hd:text-6xl 4k2:text-9xl font-playfair text-transparent font-bold 
-                                                bg-clip-text bg-gradient-to-l from-[#5857F6] to-[#101052]">
-                                                    Why Contact us?
-                                                </h1>
-                              <ul className="gap-2">
-                                <li>Personalised Solutions</li>
-                                <li>Expert Guidance</li>
-                                <li>No Obligation</li>
-                                <li>Quick Resopnse Time</li>
-                              </ul>
-            </div>
-    </div>
-        
-  
-</div>
+            <button className="font-raleway w-[40%] 4k2:text-5xl hd:text-2xl text-white rounded-lg bg-black py-2 px-2 4k2:py-6">
+              Start Live Chat Now
+            </button>
+          </div>
 
+          <div className="why_contact text-black gap-3  ">
+            <h1
+              className="text-2xl md:text-3xl lg:text-4xl hd:text-6xl 4k2:text-9xl font-playfair text-transparent font-bold 
+                                                bg-clip-text bg-gradient-to-l from-[#5857F6] to-[#101052]"
+            >
+              Why Contact us?
+            </h1>
+            <ul className="gap-2">
+              <li>Personalised Solutions</li>
+              <li>Expert Guidance</li>
+              <li>No Obligation</li>
+              <li>Quick Resopnse Time</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,69 +1,107 @@
-import React from 'react'
+import React from "react";
 
 function Form() {
   return (
     <>
-    <div className=" border border-white w-[80%] max-sm:w-[98%] max-lg:w-[80%] max-md:w-[80%] 4k2:w-[60%] h-[55%] max-sm:h-[70%] max-md:h-[80%] 4k2:h-[60%] z-10 relative bg-white text-black rounded-3xl flex flex-col justify- e items-start pl-8 py-8 4k2:pl-16 4k2:py-16 gap-5 max-sm:gap-0  ">
+      <div className=" border border-white w-[80%] max-sm:w-[98%] max-lg:w-[80%] max-md:w-[100%] xl:w-[70%] hd:w-[70%]  4k2:w-[70%] h-full  z-10 relative bg-white text-black rounded-3xl flex flex-col justify- e items-start pl-8 py-8 4k2:pl-16 4k2:py-16 gap-5 max-sm:gap-0  ">
+        <div className="flex flex-col gap-1 max-sm:gap-4 max-sm:h-[20%] max-md:h-[20%] max-lg:h-[20%] hd:h-[20%] max-sm:w-[80%] max-md:w-[80%] 4k2:gap-5">
+          <h1 className="max-sm:text-2lg max-lg:text-lg hd:text-3xl 4k2:text-6xl">
+            {" "}
+            <b>Contact Form</b>
+          </h1>
+          <p className="max-sm:text-sm max-lg:text-lg hd:text-2xl hd:w-[80%] 4k2:text-5xl">
+            Please fill out the form below, and we'll get back to you within 24
+            hours
+          </p>
+        </div>
 
-      <div className='flex flex-col gap-1max-sm:gap-4 max-sm:h-[20%] max-md:h-[20%] max-lg:h-[20%] hd:h-[20%] max-sm:w-[80%] max-md:w-[80%] 4k2:gap-5'>
-            <h1 className='max-sm:text-2lg max-lg:text-lg hd:text-3xl 4k2:text-6xl'> <b>Contact Form</b></h1>
-            <p className='max-sm:text-sm max-lg:text-lg hd:text-2xl hd:w-[80%] 4k2:text-5xl'>Please fill out the form below, and we'll get back to you within 24 hours</p>
-      </div>
-        
-                <div className='input_txt_container flex flex-row flex-wrap gap-7 h-[80%] max-sm:h-[80%] max-md:h-[80%] max-lg:h-[80%] hd:h-[80%] max-lg:gap-7 hd:gap-20'>
+        <div className="input_txt_container flex flex-row flex-wrap gap-7 h-[80%] max-sm:h-[80%] max-md:h-[80%] max-lg:h-[80%] hd:h-[80%] max-lg:gap-7 hd:gap-20">
+          <div className="input_container flex flex-col h-auto justify-between max-sm:gap-1 ">
+            <div className="flex flex-col h-auto hd:gap-3 ">
+              <label className="font-raleway max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl">
+                Full Name
+              </label>
+              <input
+                type="text"
+                className="border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded"
+              />
+            </div>
 
-                        <div className="input_container flex flex-col h-auto justify-between max-sm:gap-1 ">
+            <div className="flex flex-col h-auto hd:gap-3 ">
+              <label className="font-raleway max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl">
+                Email
+              </label>
+              <input
+                type="text"
+                className="border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded"
+              />
+            </div>
 
-                              <div className='flex flex-col h-auto hd:gap-3 '>
-                                <label htmlFor="" className='font-raleway max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>Full Name</label>
-                                <input type="text" className='border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded'/>
-                              </div>
+            <div className="flex flex-col h-auto hd:gap-3 ">
+              <label className="font-raleway  max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl">
+                Phone Number
+              </label>
+              <input
+                type="text"
+                className="border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded"
+              />
+            </div>
 
-                                      <div className='flex flex-col h-auto hd:gap-3 '>
-                                        <label htmlFor="" className='font-raleway max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>Email</label>
-                                        <input type="text" className='border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded'/>
-                                      </div>
+            <div className="flex flex-col h-auto hd:gap-3 ">
+              <label className="font-raleway max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl">
+                Company/Organisation
+              </label>
+              <input
+                type="text"
+                className="border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded"
+              />
+            </div>
+          </div>
 
-                                            <div className='flex flex-col h-auto hd:gap-3 '>
-                                              <label htmlFor="" className='font-raleway  max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>Phone Number</label>
-                                              <input type="text" className='border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded'/>
-                                            </div>
+          <div className="txt_container flex flex-col justify-between gap-2 max-sm:gap-1 max-md:gap-2  max-lg:gap-2 max-sm:w-[90%]">
+            <p className="font-raleway max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl">
+              interested in:
+            </p>
+            <p className="font-raleway max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl">
+              Select all that apply
+            </p>
 
-                              <div className='flex flex-col h-auto hd:gap-3 '>
-                                <label htmlFor="" className='font-raleway max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>Company/Organisation</label>
-                                <input type="text" className='border border-gray-400 w-52 hd:w-[100%] hd:h-10 4k2:h-[4rem] rounded'/>
-                              </div>
-                        </div>
+            <div className=" flex gap-1 items-center justify-start">
+              <input type="checkbox" />
+              <p>
+                <b className="font-raleway max-sm:text-base hd:text-2xl 4k2:text-5xl">
+                  Customised Transcription Services
+                </b>
+              </p>
+            </div>
 
-                        <div className="txt_container flex flex-col justify-between max-sm:gap-1 max-sm:w-[90%]">
-                          <p className='font-raleway max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>interested in:</p>
-                          <p className='font-raleway max-sm:text-2lg max-lg:text-lg hd:text-2xl 4k2:text-5xl'>Select all that apply</p>
-                          
-                              <div className=' flex gap-1 items-center justify-start'>
-                                  <input type='checkbox'  />
-                                  <p><b className='font-raleway max-sm:text-base hd:text-2xl 4k2:text-5xl'>Customised Transcription Services</b></p>
-                              </div>
+            <div className="flex gap-1 items-center justify-start">
+              <input type="checkbox" />
+              <p>
+                <b className="font-raleway max-sm:text-base hd:text-2xl 4k2:text-5xl">
+                  Request a Demo
+                </b>
+              </p>
+            </div>
 
-                                    <div className='flex gap-1 items-center justify-start' >
-                                        <input type='checkbox' />
-                                      <p><b className='font-raleway max-sm:text-base hd:text-2xl 4k2:text-5xl'>Request a Demo</b></p>
-                                    </div>
+            <div className=" flex gap-1 items-center justify-start">
+              <input type="checkbox" />
+              <p>
+                <b className="font-raleway max-sm:text-base hd:text-2xl 4k2:text-5xl">
+                  Other
+                </b>
+              </p>
+            </div>
+            <textarea className="w-40 h-15 sm:w-60 sm:h-28 md:w-80 md:h-36 lg:w-full lg:h-20 resize-none border-[1px] border-solid border-gray-800 rounded-md"></textarea>
 
-                                          <div className=' flex gap-1 items-center justify-start'>
-                                              <input type='checkbox' />
-                                            <p><b className='font-raleway max-sm:text-base hd:text-2xl 4k2:text-5xl'>Other</b></p>
-                                          </div>
-                          <textarea className='text-black border border-gray-500 rounded 4k2:h-[9rem]' cols="auto" rows="auto"></textarea>
-                          
-                          <button className='font-raleway w-[40%] 4k2:text-5xl hd:text-2xl text-white rounded-lg bg-black py-2 px-2 4k2:py-6'>Submit</button>
-
-
-                        </div>
-
-                </div>
+            <button className="font-raleway w-[40%] 4k2:text-5xl hd:text-2xl text-white rounded-lg bg-black py-2 px-2 4k2:py-6">
+              Submit
+            </button>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Form
+export default Form;
