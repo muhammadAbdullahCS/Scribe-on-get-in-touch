0ca6f14b18076cf71731c2ffbc831b721db2f83a
muhammadAbdullahCS <muhammad.abdullah.cs01@gmail.com>
2024-12-14 13:33:42 +0500

    responsive till key features done

---
+++ src/components/FeatureComp.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import BlobAnimation from "./BlobAnimation";
+import Arrow from "../assets/arrow.png";
+
+function FeatureComp({
+  btnHeading,
+  headingst,
+  paragraphst,
+  headingnd,
+  paragraphnd,
+  headingrd,
+  paragraphrd,
+}) {
+  const blobs = [
+    {
+      color: "#6DC6FE",
+      size: "10.42vw",
+      position: { top: "4.63vh", left: "25vw" },
+      type: "down",
+      speed: 3,
+    },
+    {
+      color: "#6DC6FE",
+      size: "5.21vw",
+      position: { top: "18.52vh", left: "42vw" },
+      type: "top",
+      speed: 5,
+    },
+    {
+      color: "#6DC6FE",
+      size: "10.42vw",
+      position: { top: "18.52vh", left: "40vw" },
+      type: "down",
+      speed: 3,
+    },
+    {
+      color: "#9B29FF",
+      size: "9.37vw",
+      position: { top: "4.63vh", left: "35vw" },
+      type: "top",
+      speed: 5,
+    },
+    {
+      color: "#9B29FF",
+      size: "9.37vw",
+      position: { top: "18.52vh", left: "30vw" },
+      type: "top",
+      speed: 4,
+    },
+    {
+      color: "#9B29FF",
+      size: "7.82vw",
+      position: { top: "4.63vh", left: "55vw" },
+      type: "down",
+      speed: 5,
+    },
+    {
+      color: "#9B29FF",
+      size: "5.21vw",
+      position: { top: "18.52vh", left: "20vw" },
+      type: "top",
+      speed: 3,
+    },
+    {
+      color: "#9B29FF",
+      size: "10.42vw",
+      position: { top: "18.52vh", left: "55vw" },
+      type: "down",
+      speed: 5,
+    },
+    {
+      color: "#6DC6FE",
+      size: "9.37vw",
+      position: { top: "13.89vh", left: "50vw" },
+      type: "top",
+      speed: 4,
+    },
+  ];
+
+  const [arrow, setArrow] = useState(false);
+  const arrowHandle = () => {
+    setArrow(!arrow);
+    console.log(arrow);
+  };
+  return (
+    <>
+      <div className="feature ">
+        <div className="flex items-center ">
+          <h1 className="text-indigo-700 max-sm:text-sm 4k2:text-5xl 4k2:py-7">
+            <b>{btnHeading}</b>
+          </h1>
+          <img
+            src={Arrow}
+            alt=""
+            onClick={arrowHandle}
+            className={arrow ? "rotate-180" : "rotate-0"}
+          />
+        </div>
+        <div className=""></div>
+
+        <div className={arrow ? "flex flex-col justify-start " : "hidden"}>
+          <div className="w-full xl:w-[60vw] rounded-3xl h-auto bg-[#0D002D] relative overflow-hidden flex justify-start items-center flex-col gap-5 pb-6">
+            <BlobAnimation blobs={blobs} style={{ zIndex: -1 }} />
+
+            <div className="flex flex-col h-full justify-start w-[90%] pt-6 4k2:pb-9 4k2:pt-14">
+              <div className="z-20">
+                <p className="text-left font-raleway text-base 4k2:text-4xl pb-4 text-white">
+                  <b>{headingst}</b>
+                  {paragraphst}
+                </p>
+                <p className="text-left font-raleway text-base 4k2:text-4xl text-white">
+                  <b>{headingnd}</b>
+                  {paragraphnd}
+                </p>
+                <p className="text-left font-raleway text-base 4k2:text-4xl text-white">
+                  <b>{headingrd}</b>
+                  {paragraphrd}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default FeatureComp;
--- src/components/Security.jsx
+++ src/components/Security.jsx
@@ -2,70 +2,42 @@ import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import security from "../assets/security.png";
-import Arrow from "../assets/arrow.png";
-import Blob from "./Blob";
+
+import FeatureComp from "./FeatureComp";
 
 function Security() {
-  const [arrow1, setArrow1] = useState(true);
-  const [arrow2, setArrow2] = useState(true);
-  const [arrow3, setArrow3] = useState(true);
-  const [arrow4, setArrow4] = useState(true);
-  const [arrow5, setArrow5] = useState(true);
-  const [arrow6, setArrow6] = useState(true);
-
-  const arrow1Handle = () => {
-    setArrow1(!arrow1);
-  };
-
-  const arrow2Handle = () => {
-    setArrow2(!arrow2);
-  };
-
-  const arrow3Handle = () => {
-    setArrow3(!arrow3);
-  };
-
-  const arrow4Handle = () => {
-    setArrow4(!arrow4);
-  };
-
-  const arrow5Handle = () => {
-    setArrow5(!arrow5);
-  };
-
-  const arrow6Handle = () => {
-    setArrow6(!arrow6);
-  };
-  // ------------blob js----------
-
   return (
     <>
-      <Navbar />
+      <Navbar mode="dark" />
       <div className="Top_State_container  flex flex-col items-center w-full h-auto py-10 gap-7">
-        <div className="lock_heading flex gap-4 w-[60%]">
-          <img src={security} className="object-cover w-[20%] h-auto" alt="" />
-          <div className="Topheading flex flex-col justify-end w-[80%] border-[2px] border-red-900 border-solid gap-5">
-            <h1 className="text-3xl font-playfair text-indigo-700 ">
+        <div className="lock_heading flex flex-wrap md:flex-nowrap justify-center gap-4 w-[60%] max-sm:w-[90%] ">
+          <img
+            src={security}
+            className="object-cover w-[20%] max-sm:w-[60%] md:w-[30%] h-auto max-sm:h-[60%]"
+            alt=""
+          />
+          <div className="Topheading flex flex-col justify-end w-[80%] max-sm:w-[90%] md:w-[70%] gap-5">
+            <h1 className="text-3xl max-sm:text-lg xl:text-5xl 4k2:text-9xl font-playfair text-indigo-700 ">
               <b>Your Data Security Is Our Top Priority</b>
             </h1>
-            <p className="font-raleway text-black">
-              We are committed to safeguarding your information with
-              industrycontent-leading security measures and protocols.
+            <p className="font-raleway text-black max-sm:text-xs xl:text-2xl 4k2:text-7xl">
+              We are committed to safeguarding your information with industry
+              content-leading security measures and protocols.
             </p>
           </div>
         </div>
 
-        <div className="below_heading w-[60%]">
-          <p className=" text-black pb-2">
+        <div className="below_heading w-[60%] max-sm:w-[80%] 4k2:pt-7">
+          <p className=" text-black pb-2 font-raleway max-sm:text-sm xl:text-xl 4k2:text-6xl">
             At Scribe On, we understand that the confidentiality, integrity, and
             availability of your data are paramount. We have implemented
             comprehensive security measures to ensure that your information is
             protected at all times.
           </p>
-          <h1 className="text-left font-playfair text-lg max-sm:text-2xl max-md:text-xl sm:text-1xl md:text-2xl lg:text-2xl xl:text-3xl hd:text-5xl 4k:text-6xl 4k:leading-snug hd:leading-tight md:leading-[1.15] 4k2:text-9xl 4k2:leading-tight w-auto mb-3 text-black text-indigo-700 ">
-            State-of-the-Art Infrastructure with AWS
+          <h1 className="text-left font-playfair text-lg max-sm:text-2xl max-md:text-xl sm:text-1xl md:text-2xl lg:text-2xl xl:text-3xl hd:text-5xl 4k:text-6xl 4k:leading-snug hd:leading-tight md:leading-[1.15] 4k2:text-9xl 4k2:leading-tight w-auto max-sm:w-[90%] mb-3 text-black text-indigo-700 ">
+            <b>State-of-the-Art Infrastructure with AWS</b>
           </h1>
-          <p className=" text-black">
+          <p className=" text-black xl:text-xl 4k2:text-6xl">
             We run on Amazon Web Services (AWS), a global leader in cloud
             infrastructure, to host our platform. AWS provides a secure,
             scalable, and reliable environment that adheres to the highest
@@ -75,115 +47,67 @@ function Security() {
       </div>
       {/* -------------------------------------------key features------------ */}
 
-      <div className="keyBenefits text-black flex flex-col items-center  h-auto">
-        <div className="container w-[60%] flex flex-col items-start gap-2">
-          <h1 className="text-3xl font-playfair text-indigo-700 ">
+      <div className="keyBenefits text-black flex flex-col items-center max-sm:items-start w-auto h-auto pt-6 pb-10">
+        <div className="container w-[60%] max-sm:w-[80%] max-sm:mx-auto flex flex-col items-start gap-2 xl:gap-4">
+          <h1 className="text-3xl xl:text-4xl 4k2:text-8xl font-playfair text-indigo-700 ">
             <b>Keys Security Features:</b>
           </h1>
-          <h1 className="text-1xl font-playfair text-indigo-700 ">
+          <h1 className="text-1xl xl:text-2xl 4k2:text-7xl font-playfair text-indigo-700">
             <b>Keys Security Features:</b>
           </h1>
-          <div className="feature-1">
-            <div className="flex items-center">
-              <h1>
-                <b>Data Encryption</b>
-              </h1>
-              <img src={Arrow} alt="" onClick={arrow1Handle} />
-            </div>
-            <div className={arrow1 ? "block text-white" : "hidden "}>
-              <Blob
-                headingst="In Transit: "
-                paragraphst="All data transmitted between your device and our servers is encrypted using Secure Socket Layer (SSL)/Transport Layer Security (TLS) protocols. This ensures that your data cannot be intercepted or read by unauthorised parties during transmission."
-                headingnd="At Rest: "
-                paragraphnd="Your data is encrypted when stored on our servers using Advanced Encryption Standard (AES) 256-bit encryption, one of the strongest encryption standards available."
-              />
-            </div>
-          </div>
+
           {/* --- */}
-          <div className="feature-1">
-            <div className="flex items-center">
-              <h1>
-                <b>Data Encryption</b>
-              </h1>
-              <img src={Arrow} alt="" onClick={arrow2Handle} />
-            </div>
-            <div className={arrow2 ? "block text-white" : "hidden "}>
-              <Blob
-                headingst="In Transit: "
-                paragraphst="All data transmitted between your device and our servers is encrypted using Secure Socket Layer (SSL)/Transport Layer Security (TLS) protocols. This ensures that your data cannot be intercepted or read by unauthorised parties during transmission."
-                headingnd="At Rest: "
-                paragraphnd="Your data is encrypted when stored on our servers using Advanced Encryption Standard (AES) 256-bit encryption, one of the strongest encryption standards available."
-              />
-            </div>
-          </div>
+          <FeatureComp
+            btnHeading="Data Encrytion"
+            headingst="In Transit: "
+            paragraphst="All data transmitted between your device and our servers is encrypted using Secure Socket Layer (SSL)/Transport Layer Security (TLS) protocols. This ensures that your data cannot be intercepted or read by unauthorised parties during transmission."
+            headingnd="At Rest: "
+            paragraphnd="Your data is encrypted when stored on our servers using Advanced Encryption Standard (AES) 256-bit encryption, one of the strongest encryption standards available."
+          />
           {/* --- */}
-          <div className="feature-3">
-            <div className="flex items-center">
-              <h1>
-                <b>Data Encryption</b>
-              </h1>
-              <img src={Arrow} alt="" onClick={arrow3Handle} />
-            </div>
-            <div className={arrow3 ? "block text-white" : "hidden "}>
-              <Blob
-                headingst="In Transit: "
-                paragraphst="All data transmitted between your device and our servers is encrypted using Secure Socket Layer (SSL)/Transport Layer Security (TLS) protocols. This ensures that your data cannot be intercepted or read by unauthorised parties during transmission."
-                headingnd="At Rest: "
-                paragraphnd="Your data is encrypted when stored on our servers using Advanced Encryption Standard (AES) 256-bit encryption, one of the strongest encryption standards available."
-              />
-            </div>
-          </div>
+          <FeatureComp
+            btnHeading="Network Security"
+            headingst="Firewalls and Access Controls: "
+            paragraphst="We employ advanced firewalls and intrusion detection/prevention systems to protect our network from unauthorised access and cyber threats."
+            headingnd="Secure Access Management: "
+            paragraphnd="Strict access controls are in place to ensure that only authorised personnel can access sensitive data, following the principle of least privilege."
+          />
           {/* --- */}
-          <div className="feature-4">
-            <div className="flex items-center">
-              <h1>
-                <b>Data Encryption</b>
-              </h1>
-              <img src={Arrow} alt="" onClick={arrow4Handle} />
-            </div>
-            <div className={arrow4 ? "block text-white" : "hidden "}>
-              <Blob
-                headingst="In Transit: "
-                paragraphst="All data transmitted between your device and our servers is encrypted using Secure Socket Layer (SSL)/Transport Layer Security (TLS) protocols. This ensures that your data cannot be intercepted or read by unauthorised parties during transmission."
-                headingnd="At Rest: "
-                paragraphnd="Your data is encrypted when stored on our servers using Advanced Encryption Standard (AES) 256-bit encryption, one of the strongest encryption standards available."
-              />
-            </div>
-          </div>
+          <FeatureComp
+            btnHeading="Compliance and Certifications"
+            headingst="AWS Compliance: "
+            paragraphst="AWS infrastructure meets numerous compliance standards, including ISO 27001, SOC 1/2/3, PCI DSS Level 1, and more. This ensures that our underlying infrastructure adheres to the highest security protocols. Regulatory Compliance: We are committed to complying with all relevant data protection regulations applicable to our services."
+            headingnd=""
+            paragraphnd=""
+          />
           {/* --- */}
-          <div className="feature-5">
-            <div className="flex items-center">
-              <h1>
-                <b>Data Encryption</b>
-              </h1>
-              <img src={Arrow} alt="" onClick={arrow5Handle} />
-            </div>
-            <div className={arrow5 ? "block text-white" : "hidden "}>
-              <Blob
-                headingst="In Transit: "
-                paragraphst="All data transmitted between your device and our servers is encrypted using Secure Socket Layer (SSL)/Transport Layer Security (TLS) protocols. This ensures that your data cannot be intercepted or read by unauthorised parties during transmission."
-                headingnd="At Rest: "
-                paragraphnd="Your data is encrypted when stored on our servers using Advanced Encryption Standard (AES) 256-bit encryption, one of the strongest encryption standards available."
-              />
-            </div>
-          </div>
+          <FeatureComp
+            btnHeading="Physical Security"
+            headingst="Secure Data Centers: "
+            paragraphst="AWS data centres are secured with 24/7 surveillance, biometric access controls, and other physical security measures to prevent unauthorised access."
+            headingnd=""
+            paragraphnd=""
+          />
           {/* --- */}
-          <div className="feature-6">
-            <div className="flex items-center">
-              <h1>
-                <b>Data Encryption</b>
-              </h1>
-              <img src={Arrow} alt="" onClick={arrow6Handle} />
-            </div>
-            <div className={arrow6 ? "block text-white" : "hidden "}>
-              <Blob
-                headingst="In Transit: "
-                paragraphst="All data transmitted between your device and our servers is encrypted using Secure Socket Layer (SSL)/Transport Layer Security (TLS) protocols. This ensures that your data cannot be intercepted or read by unauthorised parties during transmission."
-                headingnd="At Rest: "
-                paragraphnd="Your data is encrypted when stored on our servers using Advanced Encryption Standard (AES) 256-bit encryption, one of the strongest encryption standards available."
-              />
-            </div>
-          </div>
+          <FeatureComp
+            btnHeading="Operational Security"
+            headingst="Regular Security Audits: "
+            paragraphst="We conduct regular security assessments and vulnerability scans to identify and address potential security risks."
+            headingnd="Employee Training: "
+            paragraphnd="Our team members undergo regular training on data security and privacy best practices."
+            headingrd="Incident Response Plan: "
+            paragraphrd="We have a comprehensive incident response plan to promptly address and mitigate any security incidents."
+          />
+          {/* --- */}
+          <FeatureComp
+            btnHeading="Data Backup and Recovery"
+            headingst="Regular Backups: "
+            paragraphst="Your data is regularly backed up to prevent loss due to unexpected events."
+            headingnd="Disaster Recovery: "
+            paragraphnd="Our disaster recovery plans ensure business continuity and quick restoration of services in the event of an outage."
+          />
+
+          {/* ---- */}
         </div>
       </div>
 
